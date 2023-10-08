80774156b443e4422bcfcc981abe3a5f2f604dfd
Yang Yang <compking@gmail.com>
2023-10-08 17:59:24 +0800

    refined rasa

--- rasa/skills/infoservice/weather/actions.py
+++ rasa/skills/infoservice/weather/actions.py
@@ -1,19 +1,20 @@
 from typing import Any, Dict, List, Text, Optional
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-import datetime
+import datetime as dt
 import os
 import requests
 import json
+import dateparser
 from requests import ConnectionError, HTTPError, TooManyRedirects, Timeout
 from dotenv import load_dotenv
 
 load_dotenv()
 KEY = os.getenv("SENIVERSE_KEY", "")  # API key
 API = "https://api.seniverse.com/v3/weather/daily.json"  # API URL
-UNIT = "c"  # 温度单位
-LANGUAGE = "zh-Hans"  # 查询结果的返回语言
-one_day_timedelta = datetime.timedelta(days=1)
+UNIT = "c" # Temperature unit
+LANGUAGE = "zh-Hans" # language
+one_day_timedelta = dt.timedelta(days=1)
 
 
 def fetch_weather(location: str, start=0, days=15) -> dict:
@@ -32,8 +33,8 @@ def fetch_weather(location: str, start=0, days=15) -> dict:
     return result.json()
 
 
-def get_weather_by_date(location: str, date: datetime.date) -> dict:
-    day_timedelta = date - datetime.datetime.today().date()
+def get_weather_by_date(location: str, date: dt.date) -> dict:
+    day_timedelta = date - dt.datetime.today().date()
     day = day_timedelta // one_day_timedelta
 
     return get_weather_by_day(location, day)
@@ -41,7 +42,7 @@ def get_weather_by_date(location: str, date: datetime.date) -> dict:
 
 def get_weather_by_day(location: str, day=1) -> dict:
     result = fetch_weather(location)
-    print(result)
+
     normal_result = {
         "location": result["results"][0]["location"],
         "result": result["results"][0]["daily"][day],
@@ -49,90 +50,102 @@ def get_weather_by_day(location: str, day=1) -> dict:
 
     return normal_result
 
+def get_current_location() -> str:
+    return "北京"
+
 
 def get_text_weather_date(
-    location: str, date_time: datetime.date, raw_date_time: str
-) -> str:
+    location: str, date_time: dt.date, raw_date_time: str
+) -> dict:
     try:
         result = get_weather_by_date(location, date_time)
     except (ConnectionError, HTTPError, TooManyRedirects, Timeout) as e:
-        text_message = "{}".format(e)
+        text_message = {}
     else:
-        text_message_tpl = "{} {} ({}) 的天气情况为：白天：{}；夜晚：{}；气温：{}-{} 度"
-        text_message = text_message_tpl.format(
-            result["location"]["name"],
-            raw_date_time,
-            result["result"]["date"],
-            result["result"]["text_day"],
-            result["result"]["text_night"],
-            result["result"]["high"],
-            result["result"]["low"],
-        )
+       
+        text_message = {
+            "location": result["location"]["name"],
+            "datetime": raw_date_time,
+            "calendartime": result["result"]["date"],
+            "text_day": result["result"]["text_day"],
+            "text_night": result["result"]["text_night"],
+            "high": result["result"]["high"],
+            "low": result["result"]["low"],
+        }
 
     return text_message
 
 
-def text_to_date(text_date: str) -> Optional[datetime.date]:
-    """convert text based Chinese date info into datatime object
+def text_to_date(text_date: str) -> Optional[dt.date]:
+    """convert text based date info into datatime object
 
     if the convert is not supprted will return None
     """
+    if not text_date:
+        date = dt.datetime.today().date()
+    else:
+        today = dt.datetime.now()
+        one_more_day = dt.timedelta(days=1)
 
-    today = datetime.datetime.now()
-    one_more_day = datetime.timedelta(days=1)
+        if text_date == "今天":
+            date = today.date()
+        if text_date == "明天":
+            date = (today + one_more_day).date()
+        if text_date == "后天":
+            date = (today + one_more_day * 2).date()
 
-    if text_date == "今天":
-        return today.date()
-    if text_date == "明天":
-        return (today + one_more_day).date()
-    if text_date == "后天":
-        return (today + one_more_day * 2).date()
+        # Not supported by weather API provider freely
+        if text_date == "大后天":
+            # return 3
+            date = (today + one_more_day * 3).date()
 
-    # Not supported by weather API provider freely
-    if text_date == "大后天":
-        # return 3
-        return (today + one_more_day * 3).date()
+        if text_date.startswith("星期"):
+            # not supported yet
+            date = None
 
-    if text_date.startswith("星期"):
-        # not supported yet
-        return None
+        if text_date.startswith("下星期"):
+            # not supported yet
+            date = None
 
-    if text_date.startswith("下星期"):
-        # not supported yet
-        return None
-
-    # follow APIs are not supported by weather API provider freely
-    if text_date == "昨天":
-        return None
-    if text_date == "前天":
-        return None
-    if text_date == "大前天":
-        return None
+        # follow APIs are not supported by weather API provider freely
+        if text_date == "昨天":
+            date =  None
+        if text_date == "前天":
+            date =  None
+        if text_date == "大前天":
+            date =  None
+    return date
 
 
-class ActionWeatherForm(Action):
+class ActionQueryWeather(Action):
     def name(self) -> Text:
-        return "action_weather_form_submit"
+        return "action_query_weather"
 
     def run(
         self, dispatch: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
     ) -> List[Dict]:
-        city = tracker.get_slot("location")
-        date_text = tracker.get_slot("datetime")
+        location = tracker.get_slot("location")
+        datetime = tracker.get_slot("datetime")
 
-        date_object = text_to_date(date_text)
+        print(location, datetime)
+
+        if not location:
+            location = get_current_location()
+
+        date_object = text_to_date(datetime)
+
+        print(date_object)
 
         if not date_object:  # parse date_time failed
-            msg = "暂不支持查询 {} 的天气".format([city, date_text])
-            dispatch.utter_message(msg)
+            dispatch.utter_message(template = "utter_no_weather_info", location = location, datetime = datetime)
         else:
             try:
                 weather_data = get_text_weather_date(
-                    city, date_object, date_text)
+                    location, date_object, datetime)
             except Exception as e:
                 exec_msg = str(e)
                 dispatch.utter_message(exec_msg)
             else:
-                dispatch.utter_message(weather_data)
+                dispatch.utter_message(template = "utter_weather_info", **weather_data)
 
         return []
